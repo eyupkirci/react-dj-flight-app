73429c46fb33b24b36a7e8118c291531eddcd502
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-20 11:12:43 +0300

    frontend | created all structer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppRouter from './router/AppRouter'
 
-import axios from 'axios';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './redux/rootReducer';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 function App() {
 
-  useEffect(() => {
-   fetchData()
-  }, [])
-
-
-  const fetchData = () => {
-    axios.get('/api/countries/')
-      .then(({data}) => console.log(data))
-
-  }
-
-
-
   return (
-    <div className="App">
-          merhaba
-    </div>
+    <Provider store={store}>
+
+      <AppRouter />
+
+    </Provider>
   );
+
 }
 
 export default App;
---
+++ src/pages/flights/Flights.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState} from 'react'
+import axios from 'axios';
+
+
+const Flights = () => {
+
+    useEffect(() => {
+        fetchData()
+       }, [])
+     
+     
+       const fetchData = () => {
+         axios.get('https://react-dj-flight-app.herokuapp.com/api/countries/')
+           .then(({data}) => console.log(data))
+     
+       }
+     
+          
+    return (
+        <div>
+            Flights
+        </div>
+    )
+}
+
+export default Flights
