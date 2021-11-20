a232ce29b38b1cce81f7d87f33191547c58b88d9
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-20 11:10:49 +0300

    frontend | created login page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Navbar,
     NavbarBrand,
@@ -16,14 +16,29 @@ import {
 
 import { Link } from 'react-router-dom'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { LogoutAction } from '../../redux/actions/LoginActions'
+
+
 const NavbarMenu = () => {
 
+    const token = useSelector(state => state.loginReducer)
+    const dispatch = useDispatch()
 
     const [navbarToggle, setNavbarToggle] = useState(false)
-    const [isLogin, setIsLogin] = useState(true)
+    const [isLogin, setIsLogin] = useState('')
+
+
+    useEffect(() => {
+        setIsLogin(token)
+    }, [token])
+
+    // useEffect(() =>{
+    //     console.log(isLogin)
+    // },[isLogin])
 
     const handleLogOut = () => {
-        setIsLogin(false)
+        dispatch(LogoutAction(''))
     }
 
 
